fc09bcc701678d578adae914c927c42ad1e133b9
Mickael Le Baillif <mlebaillif@hi-media.com>
2014-04-21 10:52:01 +0200

    version bump for Pypi

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='freebox_v5_status',
-    version='1.0.0',
+    version='1.0.1',
     author='Mickael Le Baillif',
     author_email='mickael.le.baillif@gmail.com',
     packages=['freebox_v5_status'],
-    scripts=['bin/freebox_to_graphite.py'],
+    scripts=['bin/freebox_to_graphite.py', 'bin/freebox_show_status.py'],
     url='http://github.com/demikl/freeboxv5-status',
     license='LICENSE.txt',
     description='Parse Freebox V5 status page',
