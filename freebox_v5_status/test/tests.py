e93342c1f8c371eb553f648b25aaefa6211a33a6
Mickaël Le Baillif <mickael.le.baillif@gmail.com>
2018-11-12 17:32:50 +0100

    tests: working dir in CircleCI

--- freebox_v5_status/test/tests.py
+++ freebox_v5_status/test/tests.py
@@ -1,6 +1,7 @@
 import freebox_v5_status.freeboxstatus
 import pprint, os
 
-fbx_sample = open("../docs/fbx_info_1.5.20.log", "r")
+print("Current working dir: " + os.getcwd())
+fbx_sample = open("docs/fbx_info_1.5.20.log", "r")
 fbx = freebox_v5_status.freeboxstatus.FreeboxStatus( externalDataFeed = fbx_sample )
 pprint.pprint(fbx.status)
