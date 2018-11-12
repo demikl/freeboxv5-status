e41a8f888d28e36d8e9af805375f587858a259e0
Mickaël Le Baillif <mickael.le.baillif@gmail.com>
2018-11-12 17:49:25 +0100

    tests

--- freebox_v5_status/test/tests.py
+++ freebox_v5_status/test/tests.py
@@ -2,6 +2,6 @@ import freebox_v5_status.freeboxstatus
 import pprint, os
 
 print("Current working dir: " + os.getcwd())
-fbx_sample = open("docs/fbx_info_1.5.20.log", "r")
+fbx_sample = open("docs/fbx_info_1.5.20.log", "rb")
 fbx = freebox_v5_status.freeboxstatus.FreeboxStatus( externalDataFeed = fbx_sample )
 pprint.pprint(fbx.status)
