6b74756f3ee643ba5b0bbe7da8a219cb14b76ef6
Mickaël Le Baillif <mickael.le.baillif@gmail.com>
2018-11-13 09:22:25 +0100

    reliquat ancien setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-from distutils.core import setup
-
-setup(
-    name='freebox_v5_status',
-    version='1.1.0',
-    author='Mickael Le Baillif',
-    author_email='mickael.le.baillif@gmail.com',
-    packages=['freebox_v5_status'],
-    scripts=['bin/freebox_to_graphite.py', 'bin/freebox_show_status.py'],
-    url='http://github.com/demikl/freeboxv5-status',
-    license='LICENSE.txt',
-    description='Parse Freebox V5 status page',
-    long_description=open('README.md').read(),
-)
-
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
